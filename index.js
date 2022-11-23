93082fd9a308fb969d13c8d9df825eeb3653335d
+ <+>
2022-11-23 23:03:13 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide =(number= 2000, number1= 100)=>{
+ return 2000/100
+}
+
+const square =(num)=>num ** 2;
+square (2 ** 2)
+
+const add= ( number2, number3)=>number2 + number3;
+add (3 + 4)
